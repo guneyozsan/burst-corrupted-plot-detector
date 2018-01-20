c58a21fa74f006a7389069c9c9ea9189f660af13
Guney Ozsan <guneyozsan@gmail.com>
2018-01-21 02:43:04 +0300

    List the files in a directory using the example came with dirent.

---
+++ main.cpp
@@ -0,0 +1,55 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "include/dirent.h"
+
+static void list_directory(const char *dirname);
+
+int main(int argc, char *argv[]) {
+	int i;
+
+	/* For each directory in command line */
+	i = 1;
+	while (i < argc) {
+		list_directory(argv[i]);
+		i++;
+	}
+
+	/* List current working directory if no arguments on command line */
+	if (argc == 1) {
+		list_directory(".");
+	}
+	return EXIT_SUCCESS;
+}
+
+/*
+* List files and directories within a directory.
+*/
+static void
+list_directory(
+	const char *dirname)
+{
+	DIR *dir;
+	struct dirent *ent;
+
+	/* Open directory stream */
+	dir = opendir(dirname);
+	if (dir != NULL) {
+
+		/* Print all files and directories within the directory */
+		while ((ent = readdir(dir)) != NULL) {
+			if (ent->d_type == DT_REG)
+			{
+				printf("%s\n", ent->d_name);
+			}
+		}
+
+		closedir(dir);
+
+	}
+	else {
+		/* Could not open directory */
+		printf("Cannot open directory %s\n", dirname);
+		exit(EXIT_FAILURE);
+	}
+}
