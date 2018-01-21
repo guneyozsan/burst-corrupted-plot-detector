fafa1e3a353755dd7bcb4d7691f1f4c27392d105
Guney Ozsan <guneyozsan@gmail.com>
2018-01-21 19:09:20 +0300

    Return file names in dir as a string vector.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <iostream>
+#include <string>
+#include <vector>
 
 #ifdef _MSC_VER
 #include "include/dirent.h"
@@ -8,49 +11,55 @@
 #include <dirent.h>
 #endif // _MSC_VER
 
-static void list_directory(const char *dirname);
+static std::vector<dirent> get_files_in_directory(const char *dirname);
 
 int main(int argc, char *argv[]) {
 	int i;
+	std::vector<dirent> files_in_dir;
 
 	/* For each directory in command line */
 	i = 1;
 	while (i < argc) {
-		list_directory(argv[i]);
+		files_in_dir = get_files_in_directory(argv[i]);
 		i++;
 	}
 
 	/* List current working directory if no arguments on command line */
 	if (argc == 1) {
-		list_directory(".");
+		files_in_dir = get_files_in_directory(".");
 	}
+
+	for (int i = 0; i < files_in_dir.size(); i++) {
+		std::cout << files_in_dir[i].d_name << std::endl;
+	}
+
 	return EXIT_SUCCESS;
 }
 
 /*
 * List files within a directory.
 */
-static void
-list_directory(const char *dirname) {
+static std::vector<dirent> get_files_in_directory(const char *dir_name) {
 	DIR *dir;
-	struct dirent *ent;
+	struct dirent *dir_entry;
+	std::vector<struct dirent> file_entries;
 
 	/* Open directory stream */
-	dir = opendir(dirname);
+	dir = opendir(dir_name);
 	if (dir != NULL) {
 		/* Print all files within the directory */
-		while ((ent = readdir(dir)) != NULL) {
-			if (ent->d_type == DT_REG)
-			{
-				printf("%s\n", ent->d_name);
+		while ((dir_entry = readdir(dir)) != NULL) {
+			if (dir_entry->d_type == DT_REG) {
+				file_entries.push_back(*dir_entry);
 			}
 		}
 
 		closedir(dir);
+		return file_entries;
 	}
 	else {
 		/* Could not open directory */
-		printf("Cannot open directory %s\n", dirname);
+		printf("Cannot open directory %s\n", dir_name);
 		exit(EXIT_FAILURE);
 	}
 }
